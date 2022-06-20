b09e7f7d5d2eee46c8f7cb2f254916fa0c6e76b7
Felipe Gauer <felipe.gattelli@edu.pucrs.br>
2022-06-20 11:03:15 -0300

    Ave

---
+++ Ave.java
@@ -0,0 +1,12 @@
+
+/**
+ * Escreva uma descrição da classe Aves aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class Ave{
+    public void nome(){
+        String aves[]=new String[9];
+    }
+}
